008a50fcb8fef2af880ca0f3853076f18cd2f9f8
Hardip Patel <hardippatel7@gmail.com>
2023-04-01 16:19:10 -0400

    user repo

--- AdminBooksPanel/Repository/UserRepository.cs
+++ AdminBooksPanel/Repository/UserRepository.cs
@@ -1,6 +1,24 @@
-﻿namespace AdminBooksPanel.Repository
+﻿using AdminBooksPanel.Models;
+using Amazon.DynamoDBv2.DataModel;
+using System.Threading.Tasks;
+
+namespace AdminBooksPanel.Repository
 {
     public class UserRepository : BaseRepository, IUserRepository
     {
+        public UserRepository(IDynamoDBContext context) : base(context)
+        {
+        }
+
+        public async Task<User> Create(User user)
+        {
+            await _context.SaveAsync(user);
+            return user;
+        }
+
+        public async Task<User> FindByUsername(string userName)
+        {
+            return await _context.LoadAsync<User>(userName);
+        }
     }
 }
